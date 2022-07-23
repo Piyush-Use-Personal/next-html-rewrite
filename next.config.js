4c32276458e432430f95643801d96e16b90d7f26
Piyush Dubey <piyush115dubey@gmail.com>
2022-07-23 21:49:18 +0530

    rewrite changes

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  async rewrites() {
+    return [
+      {
+        source: '/',
+        destination: '/html/index.html',
+      },
+    ]
+  }
 }
 
 module.exports = nextConfig
